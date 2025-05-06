4f0fb7252aa7d4e205acdb3c3c8c205907a5df4f
DmitryTomat <turyatkod@mail.ru>
2025-05-06 14:50:00 +0500

    check1 import view research

--- mysite/myapp/api/urls.py
+++ mysite/myapp/api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('register/', api_views.api_register, name='api_register'),
     path('profile/', api_views.api_profile, name='api_profile'),
     path('profile/update/', api_views.api_update_profile, name='api_update_profile'),
+    path('research/', api_views.api_research_list, name='api_research_list'),
+    path('research/<int:research_id>/', api_views.api_research_detail, name='api_research_detail'),
 ]
